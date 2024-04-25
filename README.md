56286b8d65990f024bae79114a2fd108b5d7e7e6
Ari Gebhardt <ari.gebhardt@cooper.edu>
2024-04-24 23:13:38 -0400

    Turned program into Flask API.

--- README.md
+++ README.md
@@ -3,6 +3,5 @@
 This is a project for ranking items (this could be books, albums, headphones,
 whatever). The goal is to have multiple users rate items and sort based on a
 combination of aggregate rating and popularity. The project currently only
-depends on psycopg2 and uses Docker Compose, just run
-`sudo docker-compose up -d` and `python -i main.py` and look at the code to
-figure out how it works.
+depends on `flask` and `psycopg2` and uses Docker Compose, just run
+`sudo docker-compose up -d` and use curl to interact with the API.
--- main.py
+++ main.py
@@ -1,46 +1,86 @@
+from flask import Flask, request, jsonify
 import psycopg2
 
 con = psycopg2.connect(
-    dbname="item",
-    user="postgres",
-    password="password",
-    host="localhost",
-    port="5432"
+    dbname="item", user="postgres", password="password", host="localhost", port="5432"
 )
 cur = con.cursor()
 
+app = Flask(__name__)
 
-def upload_item(name: str, description: str) -> int:
-    cur.execute('''
-        INSERT INTO item (name, description, rating_value, rating_count) VALUES (%s, %s, %s, %s)
-    ''', (name, description, 0., 0))
+
+# Helper Functions
+def items_to_json(items):
+    return jsonify(
+        {
+            "items": [
+                {
+                    "id": item[0],
+                    "name": item[1],
+                    "description": item[2],
+                    "rating_value": item[3],
+                    "rating_count": item[4],
+                }
+                for item in items
+            ]
+        }
+    )
+
+
+# Routing
+@app.route("/upload_item", methods=["POST"])
+def upload_item():
+    data = request.json
+    cur.execute(
+        """
+        INSERT INTO item (name, description, rating_value, rating_count) VALUES (%s, %s, %s, %s) RETURNING id
+    """,
+        (data["name"], data["description"], 0.0, 0),
+    )
     con.commit()
-    return cur.lastrowid #wrong
+    return jsonify({"id": cur.fetchone()[0]})  # wrong
 
-def delete_item(id: int) -> None:
-    cur.execute('DELETE FROM item WHERE id = %s', (id,))
+
+@app.route("/delete_item", methods=["POST"])
+def delete_item():
+    data = request.json
+    cur.execute("DELETE FROM item WHERE id = %s", (data["id"],))
     con.commit()
+    return jsonify({})
 
-def apply_rating(id: int, rating: int) -> None:
+
+@app.route("/apply_rating", methods=["POST"])
+def apply_rating():
+    data = request.json
+    rating, id = data["rating"], data["id"]
     if not 1 <= rating <= 10:
         raise TypeError("Rating must be an integer between 1 and 10.")
-    cur.execute('SELECT rating_value, rating_count FROM item WHERE id = %s', (id,))
+    cur.execute("SELECT rating_value, rating_count FROM item WHERE id = %s", (id,))
     previous_rating, count = cur.fetchone()
     new_rating = (previous_rating * count + rating) / (count + 1)
-    cur.execute('UPDATE item SET rating_value = %s, rating_count = %s WHERE id = %s', (new_rating, count+1, id))
+    cur.execute(
+        "UPDATE item SET rating_value = %s, rating_count = %s WHERE id = %s",
+        (new_rating, count + 1, id),
+    )
     con.commit()
+    return jsonify({})
 
+
+@app.route("/ranking/popularity", methods=["GET"])
 def sort_by_popularity():
-    cur.execute('SELECT * FROM item ORDER BY rating_count DESC')
-    return cur.fetchall()
+    cur.execute("SELECT * FROM item ORDER BY rating_count DESC")
+    return items_to_json(cur.fetchall())
 
+
+@app.route("/ranking/quality", methods=["GET"])
 def sort_by_quality():
-    cur.execute('SELECT * FROM item ORDER BY rating_value DESC')
-    return cur.fetchall()
+    cur.execute("SELECT * FROM item ORDER BY rating_value DESC")
+    return items_to_json(cur.fetchall())
 
 
 if __name__ == "__main__":
-    cur.execute('''
+    cur.execute(
+        """
         CREATE TABLE IF NOT EXISTS item (
             id SERIAL PRIMARY KEY,
             name TEXT,
@@ -48,5 +88,6 @@ if __name__ == "__main__":
             rating_value FLOAT,
             rating_count INTEGER
         )
-        '''
+        """
     )
+    app.run(debug=True)
