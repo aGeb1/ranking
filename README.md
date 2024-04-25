461def3ff9a4610e0b6eb96a688fa38f9d1e416c
Ari Gebhardt <ari.gebhardt@cooper.edu>
2024-04-24 21:10:41 -0400

    Switched to PostgreSQL running in Docker.

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 This is a project for ranking items (this could be books, albums, headphones,
 whatever). The goal is to have multiple users rate items and sort based on a
 combination of aggregate rating and popularity. The project currently only
-depends on NumPy, just run `python -i main.py` and look at the code to figure
-out how it works.
+depends on psycopg2 and uses Docker Compose, just run
+`sudo docker-compose up -d` and `python -i main.py` and look at the code to
+figure out how it works.
--- main.py
+++ main.py
@@ -1,27 +1,33 @@
-import sqlite3
+import psycopg2
 
-con = sqlite3.connect("item.db")
+con = psycopg2.connect(
+    dbname="item",
+    user="postgres",
+    password="password",
+    host="localhost",
+    port="5432"
+)
 cur = con.cursor()
 
 
 def upload_item(name: str, description: str) -> int:
     cur.execute('''
-        INSERT INTO item (name, description, rating_value, rating_count) VALUES (?, ?, ?, ?)
+        INSERT INTO item (name, description, rating_value, rating_count) VALUES (%s, %s, %s, %s)
     ''', (name, description, 0., 0))
     con.commit()
-    return cur.lastrowid
+    return cur.lastrowid #wrong
 
 def delete_item(id: int) -> None:
-    cur.execute('DELETE FROM item WHERE id = ?', (id,))
+    cur.execute('DELETE FROM item WHERE id = %s', (id,))
     con.commit()
 
 def apply_rating(id: int, rating: int) -> None:
     if not 1 <= rating <= 10:
         raise TypeError("Rating must be an integer between 1 and 10.")
-    cur.execute('SELECT rating_value, rating_count FROM item WHERE id = ?', (id,))
+    cur.execute('SELECT rating_value, rating_count FROM item WHERE id = %s', (id,))
     previous_rating, count = cur.fetchone()
     new_rating = (previous_rating * count + rating) / (count + 1)
-    cur.execute('UPDATE item SET rating_value = ?, rating_count = ? WHERE id = ?', (new_rating, count+1, id))
+    cur.execute('UPDATE item SET rating_value = %s, rating_count = %s WHERE id = %s', (new_rating, count+1, id))
     con.commit()
 
 def sort_by_popularity():
@@ -29,14 +35,14 @@ def sort_by_popularity():
     return cur.fetchall()
 
 def sort_by_quality():
-    cur.execute('SELECT * FROM item ORDER BY rating_valueq DESC')
+    cur.execute('SELECT * FROM item ORDER BY rating_value DESC')
     return cur.fetchall()
 
 
 if __name__ == "__main__":
     cur.execute('''
         CREATE TABLE IF NOT EXISTS item (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id SERIAL PRIMARY KEY,
             name TEXT,
             description TEXT,
             rating_value FLOAT,
